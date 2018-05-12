4b1cf6e575cd40ab19bf7513ee556f8f06e8a9ef
eirikhl <eirikhl@stud.ntnu.no>
2018-05-12 18:30:15 +0200

    Started tp2

---
+++ tp2/tp2.c
@@ -0,0 +1,35 @@
+int miss_rate;
+unsigned char cache[32][2];
+
+
+void init()
+{
+	miss_rate = 0;
+}
+
+
+unsigned char read_byte( int address )
+{
+	
+}
+
+
+int write_byte( int address, unsigned char value )
+{
+	return -1;
+}
+
+
+unsigned int get_miss_rate()
+{
+
+}
+
+
+int main( int argc, char* argv )
+{
+	
+
+	
+	init();
+}
